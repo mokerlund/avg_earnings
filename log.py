d2ba3c54363d9ed6e409e6b3173623e26fae4fe6
Megan Okerlund <okerlund.megan@gmail.com>
2019-02-03 13:39:37 -0800

    Re-organized log.py and added descriptive comments

--- log.py
+++ log.py
@@ -4,6 +4,8 @@ import pandas as pd
 filename = 'work_log.csv'
 df = pd.read_csv(filename, delimiter=',')
 
+# Overall of Averages and Totals
+
 clms = df.loc[:, ['Day of the Week', 'Hours', 'Salary', 'Tips']]
 allmean = pd.DataFrame.mean(clms)
 
@@ -12,11 +14,23 @@ meanhours = allmean[0]
 meansalary = allmean[1]
 meantips = allmean[2]
 
-sunday = clms[clms['Day of the Week'].isin(['Sunday'])]
-
-tuesday = clms[clms['Day of the Week'].isin(['Tuesday'])]
-print(sunday, tuesday)
+# print(weekday)
+# print(sunday, tuesday)
 
 # print("I worked for a total of " totalhours " hours, or an average of " meanhours " hours.")
 # print("I earntad a total of $" totalsalary " in salary, or an average of $" meansalary " per day.")
 # print("I earned a total of $" totaltips " in tips, or an average of " meantips " per day.")
+
+# Averages and Totals by Days of the week
+
+sunday = clms[clms['Day of the Week'].isin(['Sunday'])]
+allsunday = pd.DataFrame.mean(sunday)
+
+sundayhours = allsunday[0]
+sundaysalary = allsunday[1]
+sundaytips = allsunday[2]
+print(sundayhours)
+print(sundaysalary)
+print(sundaytips)
+
+weekday = clms[clms['Day of the Week'].isin(['Monday', 'Tuesday', 'Wednesday',  'Thursday', 'Friday'])]
