1ad877442322d64f4b3f2c08930c601c786cdc1e
Megan Okerlund <okerlund.megan@gmail.com>
2019-02-09 14:24:31 -0800

    Code reduced, will make 2 new graphs next and style first graph

--- log.py
+++ log.py
@@ -6,54 +6,31 @@ plt.rcdefaults()
 filename = 'work_log.csv'
 df = pd.read_csv(filename, delimiter=',')
 
-# Overall of Averages and Totals
+# Overall of Averages and Totals, [0]=HOURS, [1]=SALARY, [2]=TIPS
 
 clms = df.loc[:, ['Day of the Week', 'Hours', 'Salary', 'Tips']]
 allmean2 = pd.DataFrame.mean(clms)
 alltotals = pd.DataFrame.sum(clms)
 allmean = allmean2.round(2)
 
-meanhours = str(allmean[0])
-meansalary = str(allmean[1])
-meantips = str(allmean[2])
-totalhours = str(alltotals[1])
-totalsalary = str(alltotals[2])
-totaltips = str(alltotals[3])
-
-# print("I worked for a total of " + totalhours + " hours, or an average of " + meanhours + " hours.")
-# print("I earntad a total of $" + totalsalary + " in salary, or an average of $" + meansalary + " per day.")
-# print("I earned a total of $" + totaltips + " in tips, or an average of $" + meantips + " per day.")
 
 # Averages and Totals by Days of the week
 
 sunday = clms[clms['Day of the Week'].isin(['Sunday'])]
 meansunday2 = pd.DataFrame.mean(sunday)
 meansunday = meansunday2.round(2)
-msunhours = str(meansunday[0])
-msunsalary = str(meansunday[1])
-msuntips = str(meansunday[2])
 sunhourly = str((meansunday[1] + meansunday[2]) / meansunday[0])
-# print('I worked an average of ' + msunhours + ' hours on Sundays. I also made an average of $' + msunsalary + ' in addition to $' + msuntips + ' in tips.')
 
 saturday = clms[clms['Day of the Week'].isin(['Saturday'])]
 meansaturday2 = pd.DataFrame.mean(saturday)
 meansaturday = meansaturday2.round(2)
-msathours = str(meansaturday[0])
-msatsalary = str(meansaturday[1])
-msattips = str(meansaturday[2])
 sathourly = str((meansaturday[1] + meansaturday[2]) / meansaturday[0])
-# print('I worked an average of ' + msathours + ' hours on Saturdays. I also made an average of $' + msatsalary + ' in addition to $' + msattips + ' in tips.')
 weekday = clms[clms['Day of the Week'].isin(['Monday', 'Tuesday', 'Wednesday',  'Thursday', 'Friday'])]
 
 meanweekday2 = pd.DataFrame.mean(weekday)
 meanweekday = meanweekday2.round(2)
-mwkhours = str(meanweekday[0])
-mwksalary = str(meanweekday[1])
-mwktips = str(meanweekday[2])
 weekhourly = str((meanweekday[1] + meanweekday[2]) / meanweekday[0])
-# print('I worked an average of ' + mwkhours + ' hours on weekdays. I also made an average of $' + mwksalary + ' in addition to $' + mwktips + ' in tips.')
 
-print()
 print('Sunday hourly rate: $' + sunhourly + '.' 'Saturday hourly rate: $' + sathourly + '.' 'Weekday hourly rate: $' + weekhourly + '.')
 
 # Matplotlib implementation
@@ -89,3 +66,4 @@ days = 3*4
 
 
 # print('If I worked for 3 months on Sunday and one weekday, I could make about $' + + '.')
+# USE HOURLY AND EARNING TOTALS FOR NEW GRAPH THAT MAKES IT EASIEST TO SEE + THE GRAPH FOR PROJECTED
